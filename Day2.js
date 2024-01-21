3c63d05724512e0d0b7befeb376b073b169baa79
Priyanka <priyanka902kumari@gmail.com>
2024-01-21 17:22:48 +0530

    update

--- Day2.js
+++ Day2.js
@@ -5,7 +5,7 @@ const arr = [
     { name: "b", age: 20 }, 
     { name: "c", age: 30 }
 ];
-
+//map
 arr.map((item) => {
     console.log(`${item.name} ${item.age}`)
     }
@@ -18,3 +18,22 @@ arr.filter((item)=>item.age>10).map((item)=>console.log(item.name));
 console.log("reducer")
 const sum =arr.reduce((acc,curr)=>acc+curr.age,0);
 console.log(sum);
+
+/* interview question
+1. Explain the basic usage of the map function in JavaScript.
+The map function in JavaScript is an array method that is used to iterate over each element of an array and 
+create a new array . 
+map(callbackFn)
+callbackFn
+A function to execute for each element in the array. Its return value is added as a single element in 
+the new array. 
+
+foreach 
+can mutate the original array
+return undifine
+does not have ability of chaining
+https://www.freecodecamp.org/news/4-main-differences-between-foreach-and-map/
+
+
+https://www.freecodecamp.org/news/javascript-map-reduce-and-filter-explained-with-examples/
+*/
